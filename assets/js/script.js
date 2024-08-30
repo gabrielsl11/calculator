ed6f204d87b51013f2cf0abf9d22dd8486efb756
Gabriel Souza <92094301+gabrielsl11@users.noreply.github.com>
2024-08-30 15:11:34 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.button').forEach(element => {
+            element.addEventListener('click', (e) => {
+                addValue(element.innerHTML);
+            })
+        });
+        
+        function addValue(value) {
+            var input = document.querySelector('#screen');
+            if (value == 'CE') {
+                input.value = '';
+            } else if (value == '=') {
+                input.value = eval(input.value);
+            } else {
+                input.value += value;
+            }
+        }
